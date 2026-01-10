363ab85f0e0d2202a566f585d23571943ed594f0
Niklas Schmeiser Laptop <schmeiserniklas@gmail.com>
2026-01-10 20:43:51 +0100

    Changed Screen selection

--- HMI/FactoryIOHMI/FactoryIOHMI/obj/Debug/net8.0-windows/Views/HomeScreen.g.i.cs
+++ HMI/FactoryIOHMI/FactoryIOHMI/obj/Debug/net8.0-windows/Views/HomeScreen.g.i.cs
@@ -53,7 +53,7 @@ namespace FactoryIOHMI.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FactoryIOHMI;component/views/homescreen.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FactoryIOHMI;V1.0.0.0;component/views/homescreen.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\HomeScreen.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- HMI/FactoryIOHMI/FactoryIOHMI/obj/Debug/net8.0-windows/Views/MainScreen.g.i.cs
+++ HMI/FactoryIOHMI/FactoryIOHMI/obj/Debug/net8.0-windows/Views/MainScreen.g.i.cs
@@ -53,7 +53,7 @@ namespace FactoryIOHMI.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FactoryIOHMI;component/views/mainscreen.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FactoryIOHMI;V1.0.0.0;component/views/mainscreen.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainScreen.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
