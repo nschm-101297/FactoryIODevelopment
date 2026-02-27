049af55c9b36b96089e40ef4e593e01555052414
Niklas Schmeiser <schmeiserniklas@gmail.com>
2026-02-27 14:16:59 +0100

    Added HMI project in TwinCAT

---
+++ Entwicklungsprojekt-FactoryIO_Beckhoff/Packages/Beckhoff.TwinCAT.HMI.Framework.14.3.393/runtimes/native1.12-tchmi/dist/API/Version.d.ts
@@ -0,0 +1,12 @@
+export declare let version: TcHmi.Version;
+/**
+ * Sets the current framework version.
+ * @param newValue The version to set.
+ */
+export declare function setVersion(newValue: TcHmi.Version): void;
+declare global {
+    namespace TcHmi {
+        let version: TcHmi.Version;
+    }
+}
+//# sourceMappingURL=Version.d.ts.map
