64bef3e4e46488bf392f50d0865fb913e6d23d61
Niklas Schmeiser <schmeiserniklas@gmail.com>
2026-05-17 14:38:44 +0200

    Added new function and variables for communication with external application via ADS communication.

--- HMI/FactoryIOHMI/FactoryIOHMI/obj/Debug/net8.0-windows/App.g.i.cs
+++ HMI/FactoryIOHMI/FactoryIOHMI/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace FactoryIOHMI {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.16.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace FactoryIOHMI {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.16.0")]
         public static void Main() {
             FactoryIOHMI.App app = new FactoryIOHMI.App();
             app.InitializeComponent();
